ca52f617648282df3e69ad0b47b1843c943cc539
james-rsp <tanapats@rentspree.com>
2025-10-17 17:13:07 +0700

    save

--- keyboards/lily58/keymaps/james/keymap.c
+++ keyboards/lily58/keymaps/james/keymap.c
@@ -11,37 +11,36 @@ enum layer_number {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
  [_COLEMAK] = LAYOUT(
-  KC_ESC,   KC_1,   KC_2,    KC_3,           KC_4,    KC_5,                                   KC_MPRV,    KC_MPLY, KC_MFFD, KC_MUTE, KC_VOLD, KC_VOLU,
-  KC_TAB,   KC_Q,   KC_W,    KC_F,           KC_P,    KC_B,                                   KC_J,       KC_L,    KC_U,    KC_Y,    KC_P,    KC_MINS,
-  KC_BSPC,  KC_A,   KC_R,    KC_S, LT(_NAVI,KC_T),    KC_G,                                   KC_H,       KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-  KC_LCTL,  KC_Z,   KC_X,    KC_C,           KC_D,    KC_V,        KC_UNDS, QK_REP,           KC_N,       KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-                          KC_LALT,        KC_LGUI, KC_LGUI, LSFT_T(KC_SPC), LT(_NAVI,KC_ENT), MO(_RAISE), KC_BSPC, KC_RGUI
-),
+  KC_ESC,   KC_1,   KC_PSLS, KC_PAST, KC_MINS, KC_PLUS,                                KC_MPRV,  KC_MPLY,          KC_MFFD, KC_MUTE, KC_VOLD, KC_VOLU,
+  KC_TAB,   KC_Q,   KC_W,    KC_F,    KC_P,    KC_B,                                   KC_J,     KC_L,             KC_U,    KC_Y,    KC_SCLN, KC_PGUP,
+  KC_BSPC,  KC_A,   KC_R,    KC_S,    KC_T,    KC_G,                                   KC_M,     LT(_SYMBOL,KC_N), KC_E,    KC_I,    KC_O,    KC_QUOT,
+  KC_CAPS,  KC_Z,   KC_X,    KC_C,    KC_D,    KC_V,        KC_MINS, QK_REP,           KC_K,     KC_H,             KC_COMM, KC_DOT,  KC_SLSH, KC_PGDN,
+                          KC_LALT, KC_LGUI, KC_LGUI, LSFT_T(KC_SPC), LT(_NAVI,KC_ENT), MO(_NUM), KC_RGUI,          KC_RGUI
+ ),
 
-[_SYMBOL] = LAYOUT(
-  _______, _______, _______, _______, _______, _______,                   _______, _______, _______,_______, _______, _______,
-  _______, KC_EXLM, KC_LBRC, KC_RBRC, KC_PEQL,  KC_GRV,                     KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
+ [_SYMBOL] = LAYOUT(
+  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+  XXXXXXX, KC_EXLM, KC_LBRC, KC_RBRC, KC_PEQL,  KC_GRV,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   KC_HASH, KC_CIRC, KC_LPRN, KC_RPRN,  KC_DLR, KC_ASTR,                   KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_TILD,
-  _______, KC_TILD, KC_LCBR, KC_RCBR, KC_COLN, KC_PERC, _______, _______, XXXXXXX, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE,
-                             _______, _______, _______, _______, _______,  _______, _______, _______
-),
+  KC_PIPE, KC_TILD, KC_LCBR, KC_RCBR, KC_COLN, KC_PERC, XXXXXXX, XXXXXXX, XXXXXXX, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE,
+                             XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
+ ),
 
-  [_NUM] = LAYOUT(
+ [_NUM] = LAYOUT(
   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   XXXXXXX, XXXXXXX,    KC_9,    KC_8,    KC_7, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   XXXXXXX, XXXXXXX,    KC_3,    KC_2,    KC_1,    KC_0,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   XXXXXXX, XXXXXXX,    KC_6,    KC_5,    KC_4, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
                              _______, _______, _______, _______, _______,  _______, _______, _______
-  )
-};
+ ),
 
-  [_NAVI] = LAYOUT(
-  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,    KC_1,    KC_0,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-  XXXXXXX, XXXXXXX, XXXXXXX,    KC_5,    KC_4, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+ [_NAVI] = LAYOUT(
+  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, LCTL(KC_LEFT), LCTL(KC_UP), LCTL(KC_RGHT), XXXXXXX, XXXXXXX,
+  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   KC_PGUP, KC_HOME,       KC_UP,       KC_END,        XXXXXXX, XXXXXXX,
+  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   KC_PGDN, KC_LEFT,       KC_DOWN,     KC_RGHT,       XXXXXXX, XXXXXXX,
+  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,       XXXXXXX,     XXXXXXX,       XXXXXXX, XXXXXXX,
                              _______, _______, _______, _______, _______,  _______, _______, _______
-  )
+ )
 };
 layer_state_t layer_state_set_user(layer_state_t state) {
   return update_tri_layer_state(state, _LOWER, _RAISE, _ADJUST);
