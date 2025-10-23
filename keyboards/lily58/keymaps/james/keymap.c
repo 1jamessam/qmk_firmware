37f3055df65f6588f87b8933c19c93ee8ffe1fca
james-rsp <tanapats@rentspree.com>
2025-10-23 20:33:24 +0700

    james layout version 1.0.0

--- keyboards/lily58/keymaps/james/keymap.c
+++ keyboards/lily58/keymaps/james/keymap.c
@@ -2,48 +2,127 @@
 
 enum layer_number {
   _COLEMAK = 0,
+  _SYMB,
+  _NUM,
   _NAVI,
-  _LOWER,
-  _RAISE,
-  _ADJUST,
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
+/* COLEMAK
+ * ,-----------------------------------------.                    ,-----------------------------------------.
+ * | ESC  | SG<- | SG-> | SG+4 |  C<- |  C-> |                    | MPRV | MPLY | MFFD | MUTE | VOLD | VOLU |
+ * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
+ * | Tab  |   Q  |   W  |   F  |   P  |   B  |                    |   J  |   L  |   U  |   Y  |   ;  | PGUP |
+ * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
+ * | BSPC |   A  |   R  |   S  |   T  |   G  |-------.    ,-------|   M  |N/SYMB| E/NUM|   I  |   O  |  DEL |
+ * |------+------+------+------+------+------|  - _  |    |  ' "  |------+------+------+------+------+------|
+ * | CAPS |   Z  |   X  |   C  |   D  |   V  |-------|    |-------|   K  |   H  |  , < |  . > |  / ? | PGDN |
+ * `-----------------------------------------/       /     \      \-----------------------------------------'
+ *                   | LAlt | LGUI | LCTL | /SFT/SPC/       \ENT/NAV\ | GU-SH| GU-CT| RALT |
+ *                   |      |      |      |/       /         \      \ |      |      |      |
+ *                   `----------------------------'           '------''--------------------'
+ */
  [_COLEMAK] = LAYOUT(
-  KC_ESC,   KC_1,   KC_PSLS, KC_PAST, KC_MINS, KC_PLUS,                                KC_MPRV,  KC_MPLY,          KC_MFFD, KC_MUTE, KC_VOLD, KC_VOLU,
-  KC_TAB,   KC_Q,   KC_W,    KC_F,    KC_P,    KC_B,                                   KC_J,     KC_L,             KC_U,    KC_Y,    KC_SCLN, KC_PGUP,
-  KC_BSPC,  KC_A,   KC_R,    KC_S,    KC_T,    KC_G,                                   KC_M,     LT(_SYMBOL,KC_N), KC_E,    KC_I,    KC_O,    KC_QUOT,
-  KC_CAPS,  KC_Z,   KC_X,    KC_C,    KC_D,    KC_V,        KC_MINS, QK_REP,           KC_K,     KC_H,             KC_COMM, KC_DOT,  KC_SLSH, KC_PGDN,
-                          KC_LALT, KC_LGUI, KC_LGUI, LSFT_T(KC_SPC), LT(_NAVI,KC_ENT), MO(_NUM), KC_RGUI,          KC_RGUI
+  KC_ESC, LSG_T(KC_LBRC), LSG_T(KC_RBRC), LSG_T(KC_4), LCTL(KC_LEFT),  LCTL(KC_RGHT),                                  KC_MPRV,       KC_MPLY,        KC_MFFD,       KC_MUTE, KC_VOLD, KC_VOLU,
+  KC_TAB,           KC_Q,           KC_W,        KC_F,          KC_P,           KC_B,                                  KC_J,          KC_L,           KC_U,          KC_Y,    KC_SCLN, KC_PGUP,
+  KC_BSPC,          KC_A,           KC_R,        KC_S,          KC_T,           KC_G,                                  KC_M,          LT(_SYMB,KC_N), LT(_NUM,KC_E), KC_I,    KC_O,    KC_DEL,
+  KC_CAPS,          KC_Z,           KC_X,        KC_C,          KC_D,           KC_V,          KC_MINS, KC_QUOT,       KC_K,          KC_H,           KC_COMM,       KC_DOT,  KC_SLSH, KC_PGDN,
+                                 KC_LALT,     KC_LGUI,       KC_LCTL, LSFT_T(KC_SPC), LT(_NAVI,KC_ENT), LGUI(KC_LSFT), LGUI(KC_LCTL), KC_RALT
  ),
 
- [_SYMBOL] = LAYOUT(
-  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+/* SYMBOL
+ * ,-----------------------------------------.                    ,-----------------------------------------.
+ * |      |      |      |   @  |   &  |   |  |                    |      |      |      |      |      |      |
+ * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
+ * |      |   !  |   [  |   ]  |   =  |   `  |                    |      |      |      |      |      |      |
+ * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
+ * |   #  |   ^  |   (  |   )  |   $  |   *  |-------.    ,-------|      |      |      |      |      |      |
+ * |------+------+------+------+------+------|   :   |    |       |------+------+------+------+------|------|
+ * |      |      |   {  |   }  |   ~  |   %  |-------|    |-------|      |      |      |      |      |      |
+ * `-----------------------------------------/       /     \      \-----------------------------------------'
+ *                   | LAlt | LGUI | LCTL | /SFT/SPC/       \ENT/NAV\ | RCTL | RGUI | RALT |
+ *                   |      |      |      |/       /         \      \ |      |      |      |
+ *                   `----------------------------'           '------''--------------------'
+ */
+ [_SYMB] = LAYOUT(
+  XXXXXXX, XXXXXXX, XXXXXXX,   KC_AT, KC_AMPR, KC_PIPE,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   XXXXXXX, KC_EXLM, KC_LBRC, KC_RBRC, KC_PEQL,  KC_GRV,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-  KC_HASH, KC_CIRC, KC_LPRN, KC_RPRN,  KC_DLR, KC_ASTR,                   KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_TILD,
-  KC_PIPE, KC_TILD, KC_LCBR, KC_RCBR, KC_COLN, KC_PERC, XXXXXXX, XXXXXXX, XXXXXXX, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE,
-                             XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
- ),
-
- [_NUM] = LAYOUT(
-  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-  XXXXXXX, XXXXXXX,    KC_9,    KC_8,    KC_7, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-  XXXXXXX, XXXXXXX,    KC_3,    KC_2,    KC_1,    KC_0,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-  XXXXXXX, XXXXXXX,    KC_6,    KC_5,    KC_4, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+  KC_HASH, KC_CIRC, KC_LPRN, KC_RPRN,  KC_DLR, KC_ASTR,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+  XXXXXXX, XXXXXXX, KC_LCBR, KC_RCBR, KC_TILD, KC_PERC, KC_COLN, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
                              _______, _______, _______, _______, _______,  _______, _______, _______
  ),
 
+/* NUM
+ * ,-----------------------------------------.                    ,-----------------------------------------.
+ * |      |      |      |      |      |      |                    |      |      |      |      |      |      |
+ * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
+ * |      |   /  |   9  |   8  |   7  |   *  |                    |      |      |      |      |      |      |
+ * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
+ * |      |   -  |   3  |   2  |   1  |   +  |-------.    ,-------|      |      |      |      |      |      |
+ * |------+------+------+------+------+------|   0   |    |       |------+------+------+------+------|------|
+ * |      |   =  |   6  |   5  |   4  |   .  |-------|    |-------|      |      |      |      |      |      |
+ * `-----------------------------------------/       /     \      \-----------------------------------------'
+ *                   | LAlt | LGUI | LCTL | /SFT/SPC/       \ENT/NAV\ | RCTL | RGUI | RALT |
+ *                   |      |      |      |/       /         \      \ |      |      |      |
+ *                   `----------------------------'           '------''--------------------'
+ */
+ [_NUM] = LAYOUT(
+  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+  XXXXXXX, KC_PSLS,    KC_9,    KC_8,    KC_7, KC_ASTR,                XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+  XXXXXXX, KC_MINS,    KC_3,    KC_2,    KC_1, KC_PLUS,                XXXXXXX, XXXXXXX,    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+  XXXXXXX, KC_PEQL,    KC_6,    KC_5,    KC_4,  KC_DOT, KC_0, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+                             _______, _______, _______, _______, _______,  _______, _______, _______
+ ),
+
+/* NAVI
+ * ,-----------------------------------------.                    ,-----------------------------------------.
+ * |      |      |      |      |      |      |                    |      | C-<- | C-^  | C->  |      |      |
+ * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
+ * |      |      |      |      |      |      |                    | PGUP | HOME |  UP  | END  |      |      |
+ * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
+ * |      |      |      |      |      |      |-------.    ,-------| PGDN | LEFT | DOWN | RGHT |      |      |
+ * |------+------+------+------+------+------|       |    |       |------+------+------+------+------|------|
+ * |      |      |      |      |      |      |-------|    |-------|      | SG-[ |      | SG-] |      |      |
+ * `-----------------------------------------/       /     \      \-----------------------------------------'
+ *                   | LAlt | LGUI | LCTL | /SFT/SPC/       \ENT/NAV\ | RCTL | RGUI | RALT |
+ *                   |      |      |      |/       /         \      \ |      |      |      |
+ *                   `----------------------------'           '------''--------------------'
+ */
  [_NAVI] = LAYOUT(
-  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, LCTL(KC_LEFT), LCTL(KC_UP), LCTL(KC_RGHT), XXXXXXX, XXXXXXX,
-  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   KC_PGUP, KC_HOME,       KC_UP,       KC_END,        XXXXXXX, XXXXXXX,
-  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   KC_PGDN, KC_LEFT,       KC_DOWN,     KC_RGHT,       XXXXXXX, XXXXXXX,
-  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,       XXXXXXX,     XXXXXXX,       XXXXXXX, XXXXXXX,
+  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, LCTL(KC_LEFT),  LCTL(KC_UP), LCTL(KC_RGHT),  XXXXXXX, XXXXXXX,
+  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   KC_PGUP, KC_HOME,        KC_UP,       KC_END,         XXXXXXX, XXXXXXX,
+  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   KC_PGDN, KC_LEFT,        KC_DOWN,     KC_RGHT,        XXXXXXX, XXXXXXX,
+  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, LSG_T(KC_LBRC), XXXXXXX,     LSG_T(KC_LBRC), XXXXXXX, XXXXXXX,
                              _______, _______, _______, _______, _______,  _______, _______, _______
  )
+
+/* THAI
+ * ,-----------------------------------------.                    ,-----------------------------------------.
+ * | ESC  |   1  |   2  |   3  |   4  |   5  |                    |   6  |   7  |   8  |   9  |   0  |  `   |
+ * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
+ * | Tab  |   Q  |   W  |   E  |   R  |   T  |                    |   Y  |   U  |   I  |   O  |   P  |  -   |
+ * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
+ * |LCTRL |   A  |   S  |   D  |   F  |   G  |-------.    ,-------|   H  |   J  |   K  |   L  |   ;  |  '   |
+ * |------+------+------+------+------+------|   [   |    |    ]  |------+------+------+------+------+------|
+ * |LShift|   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   /  |RShift|
+ * `-----------------------------------------/       /     \      \-----------------------------------------'
+ *                   | LAlt | LGUI |LOWER | /Space  /       \Enter \  |RAISE |BackSP| RGUI |
+ *                   |      |      |      |/       /         \      \ |      |      |      |
+ *                   `----------------------------'           '------''--------------------'
+ */
+
+//  [_THAI] = LAYOUT(
+//   _______,  UC(L'ภ'), UC(L'ถ'), UC(L'ุ'), UC(L'ึ'),UC(L''),                     UC(L''),    UC(L''),    UC(L''),    UC(L''),    UC(L''),    UC(L''),
+//   _______,  UC(L'ๆ'), UC(L'ไ'), UC(L'ำ'), UC(L'พ'),    UC(L'ะ'),                     UC(L''),    UC(L''),    UC(L''),    UC(L''),    UC(L''),    UC(L''),
+//   _______,  UC(L'ฟ'), UC(L'ห'), UC(L'ก'), UC(L'ด'), UC(L'เ'),                     UC(L''),    UC(L''),    UC(L''),    UC(L''),    UC(L''), UC(L''),
+//   _______,  UC(L'ผ'), UC(L'ป'), UC(L'แ'), UC(L'อ'), UC(L'ิ'), UC(L''), UC(L''),  UC(L''),    UC(L''),    UC(L''), UC(L''),  UC(L''),  UC(L''),
+//                                  _______, _______, _______, _______, _______,  _______, _______, _______
+// ),
 };
+
 layer_state_t layer_state_set_user(layer_state_t state) {
-  return update_tri_layer_state(state, _LOWER, _RAISE, _ADJUST);
+  return update_tri_layer_state(state, _SYMB, _NUM, _NAVI);
 }
 
 //SSD1306 OLED update loop, make sure to enable OLED_ENABLE=yes in rules.mk
