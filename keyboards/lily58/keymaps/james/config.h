0327731a79241452746d1c84bea79b41595565ef
james-rsp <tanapats@rentspree.com>
2025-10-29 14:42:38 +0700

    update

--- keyboards/lily58/keymaps/james/config.h
+++ keyboards/lily58/keymaps/james/config.h
@@ -24,7 +24,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define FLOW_TAP_TERM 100
 
 #define PERMISSIVE_HOLD
-#define CHORDAL_HOLD
+// #define CHORDAL_HOLD
 
 #undef RGBLIGHT_LED_COUNT
 #define RGBLIGHT_EFFECT_BREATHING
